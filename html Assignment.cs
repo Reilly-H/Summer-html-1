1385f573a2b95b1be30d3d1d4606c5b73c89b328
Reilly-H <169121151+Reilly-H@users.noreply.github.com>
2024-05-09 11:19:34 -0400

    Add files via upload

---
+++ html Assignment.cs
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>week 1 
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="discription" content="This is the code for our week 1 class">
+        <meta name="robots" content="noindex, nofollow">
+    </head>
+    <body>
+        <!-- lets add our main heading content element -->
+        <h1>I am a main heading element and there can be only one</h1>
+        <h2>Main Sub-Heading</h2>
+        <p>bchdbvhhfvbhjdbvhjfgvfbrhvbhfjdugdhjcvbdhjsvhdfvjhdbvhjbdfhvhdjbvhdvhjfdvhjbdjhcbdjhkvhjdfvhjbvhjcbvhjdbvhjdhjbchbxhvbjvbxcb</p>
+        <ol>
+            <li>Iten 1</li>
+            <li>Iten 2</li>
+        </ol>
+        <ul>
+            <li>Iten 1</li>
+            <li>Iten 2</li>
+        </ul>
+        <blockquote cite="somewebsite">fhdgvfdvhfbvhfbdhvgfdhjvbjhdfhfbv</blockquote>
+        <p>hfhhduhguvbfhjbhghufb<span>vhjfyuvgwhjegbrugy</span>rhfvjfduyrgurevhgfyu</p>
+        <p>look no need for a br</p>
+    </body>
+</html>
